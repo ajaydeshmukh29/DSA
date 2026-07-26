25bf9dbda0d9c900878dac8590eec27e2fbf34ad
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-07-26 16:30:01 +0530

    Singly Linked List Insert Last Function

---
+++ C/Linked_List/SinglyLL_INSERTLAST.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node 
+{
+  int data;
+  struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE first, int iNo)
+{
+  PNODE newn = NULL;
+
+  newn = (PNODE)malloc(sizeof(NODE));
+
+  newn->data = iNo;
+  newn->next = NULL;
+
+  if(*first == NULL)
+  {
+    *first = newn;
+  }
+  else
+  {
+     newn->next = *first;
+    *first = newn;    
+  }
+}
+
+void InsertLast(PPNODE first, int iNo)
+{
+  PNODE newn = NULL;
+  PNODE temp = NULL;
+
+  newn->data = iNo;
+  newn->next = NULL;
+
+  if(*first == NULL)
+  {
+    *first = newn;
+  }
+  else
+  {
+    temp = *first;
+
+    while(temp->next != NULL)
+    {
+      temp = temp->next;
+    }
+    temp->next = newn;
+  }
+}
+
+int main()
+{
+  PNODE head = NULL;
+  int iRet = 0;
+
+  InsertFirst(&head, 51);
+  InsertFirst(&head, 21);
+  InsertFirst(&head, 11);
+
+  InsertLast(&head, 101);
+  InsertLast(&head, 111);
+  InsertLast(&head, 121);
+
+  return 0;
+}
